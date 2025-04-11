c4c86d51d256ad105bfe0f5a5229128fd0c5b59a
soumyabrata maity <soumyabratam6@gmail.com>
2025-04-11 20:22:17 +0530

    all file added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name,setName] = useState('')
+  const handerChange = (event)=>{
+    setName(event.target.value)
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="container">
+      <h1>Personalized Greeting</h1>
+        <input 
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={handerChange}
+        className="input"
+         />
+         {name && <h2>Hello, {name}!</h2>}
     </div>
   )
 }
